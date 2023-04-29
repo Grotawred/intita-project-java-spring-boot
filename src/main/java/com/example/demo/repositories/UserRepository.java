209a3dfb15dde791d0225d12d0a6305eb82541ba
Grotenred <grishavlacko@gmai.com>
2023-04-29 21:38:56 +0300

    userRepo add

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repositories;
 
 import com.example.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
