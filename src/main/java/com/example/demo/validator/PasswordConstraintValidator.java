8fba576c07ebdbfd3aafb0ae29b623b4ba7b4471
Grish <grishavlasko200@gmail.com>
2023-07-22 15:53:59 +0300

    README file

--- src/main/java/com/example/demo/validator/PasswordConstraintValidator.java
+++ src/main/java/com/example/demo/validator/PasswordConstraintValidator.java
@@ -4,14 +4,35 @@ import com.example.demo.validator.annotation.ValidPassword;
 import com.google.common.base.Joiner;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.passay.*;
-import org.springframework.stereotype.Component;
+import org.passay.AlphabeticalSequenceRule;
+import org.passay.DigitCharacterRule;
+import org.passay.LengthRule;
+import org.passay.NumericalSequenceRule;
+import org.passay.PasswordData;
+import org.passay.PasswordValidator;
+import org.passay.QwertySequenceRule;
+import org.passay.RuleResult;
+import org.passay.SpecialCharacterRule;
+import org.passay.UppercaseCharacterRule;
+import org.passay.WhitespaceRule;
 
 import java.util.Arrays;
-import java.util.StringJoiner;
 
-@Component
+
 public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+
+    private final PasswordValidator validator = new PasswordValidator(Arrays.asList(
+            new LengthRule(8, 30),
+            new UppercaseCharacterRule(1),
+            new DigitCharacterRule(1),
+            new SpecialCharacterRule(1),
+            new NumericalSequenceRule(3, false),
+            new AlphabeticalSequenceRule(3, false),
+            new QwertySequenceRule(3, false),
+            new WhitespaceRule()
+    ));
+
+
     @Override
     public void initialize(ValidPassword arg0) {
         ConstraintValidator.super.initialize(arg0);
@@ -19,24 +40,15 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
-        PasswordValidator validator = new PasswordValidator(Arrays.asList(
-                new LengthRule(8, 30),
-                new UppercaseCharacterRule(1),
-                new DigitCharacterRule(1),
-                new SpecialCharacterRule(1),
-                new NumericalSequenceRule(3,false),
-                new AlphabeticalSequenceRule(3,false),
-                new QwertySequenceRule(3,false),
-                new WhitespaceRule()
-        ));
+        boolean isValid = true;
         RuleResult result = validator.validate(new PasswordData(password));
-        if(result.isValid()){
-            return true;
+        if (!result.isValid()) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(
+                            Joiner.on(",").join(validator.getMessages(result)))
+                    .addConstraintViolation();
+            isValid = false;
         }
-        context.disableDefaultConstraintViolation();
-        context.buildConstraintViolationWithTemplate(
-                Joiner.on(",").join(validator.getMessages(result)))
-                .addConstraintViolation();
-        return false;
+        return isValid;
     }
 }
