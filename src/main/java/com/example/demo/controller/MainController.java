c136b529071b15c2d457a35a77df39f95f2a1fc4
Grish <grishavlasko200@gmail.com>
2023-08-09 21:46:30 +0300

    pagination tests

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,27 +1,29 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Post;
-import com.example.demo.repository.PostRepository;
+import com.example.demo.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.SortDefault;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.print.Pageable;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/main")
 public class MainController {
-    private final PostRepository postRepository;
 
-    @GetMapping("/posts/page")
-    Page<Post> loadCharactersPage(
-            @PageableDefault
-            @SortDefault(sort = "id", direction = Sort.Direction.DESC)
-            Pageable pageable){
-        return postRepository.findAllPage(pageable);
+    private final MainService mainService;
+
+    @GetMapping
+    public ResponseEntity<Page<Post>> getAllPosts(@RequestParam(defaultValue = "0") int page,
+                                                  @RequestParam(defaultValue = "10") int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Post> posts = mainService.getAllPosts(pageable);
+        return ResponseEntity.ok(posts);
     }
 }
