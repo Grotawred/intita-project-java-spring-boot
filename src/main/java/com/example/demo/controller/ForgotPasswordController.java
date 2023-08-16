5ac2e9dd352cc4032be8c875229019bad76bc3be
wairixx <mehanichevaoleksandra@gmail.com>
2023-08-17 00:22:15 +0300

    changed controller

--- src/main/java/com/example/demo/controller/ForgotPasswordController.java
+++ src/main/java/com/example/demo/controller/ForgotPasswordController.java
@@ -1,21 +1,15 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.ResetToken;
-import com.example.demo.repository.UserRepository;
 import com.example.demo.service.ResetTokenService;
 import com.example.demo.model.dto.UserDTO;
 import com.example.demo.service.UserService;
-import com.example.demo.validator.annotation.ValidPassword;
+import com.example.demo.validator.PasswordConstraintValidator;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import static com.example.demo.constants.TextConstants.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -26,50 +20,55 @@ public class ForgotPasswordController {
 
     private final UserService userService;
     private final ResetTokenService resetTokenService;
+    private final PasswordConstraintValidator passwordConstraintValidator;
 
+    @GetMapping("/forgot_password")
+    private String processForgotPasswordPage() {
+        return "html_page";
+    }
 
     @PostMapping("/forgot_password")
     private String processForgotPassword(HttpServletRequest httpServletRequest) {
         String email = httpServletRequest.getParameter("email");
         String token = UUID.randomUUID().toString();
-
         List<UserDTO> userDTO = userService.findByEmail(email);
-        if (userDTO != null) {
-            resetTokenService.createToken(token, userDTO.get(0));
-            String link = "http://localhost/reset_password?token=" + token;
-            //send
-            return "";
 
-        } else {
-            return "";
+        if (userService.checkIfUserExistByEmail(email)) {
+            resetTokenService.saveToken(token, userDTO.get(0));
+            resetTokenService.createAndSendResetLink(email, token);
+            return "html_page";
         }
-
+        return "html_page_error";
     }
 
     @GetMapping("/reset_password")
-    private String resetPassword(@RequestParam("token") String token) {
-        ResetToken resetToken = resetTokenService.getByToken(token);
+    private String processResetPasswordPage(HttpServletRequest request) {
+        String token = request.getParameter("token");
 
-        if (resetToken != null) {
-            return "";
+        if (userService.checkIfUserExistByToken(token)
+                && resetTokenService.validateToken(token)) {
+            return "html_page";
         }
-
-        return "";
+        return "html_page_error";
     }
 
     @PostMapping("/reset_password")
     private String processResetPassword(HttpServletRequest request) {
         String token = request.getParameter("token");
         String password = request.getParameter("password");
+        String confirmPassword = request.getParameter("confirmPassword");
 
 
-        UserDTO userDTO = userService.getByResetToken(token);
-        if (userDTO != null) {
-            userDTO.setPassword(password);
-            userService.save(userDTO);
-            return "";
+        if (userService.checkIfUserExistByToken(token) && resetTokenService.validateToken(token)) {
+            if (resetTokenService.confirmPassword(password, confirmPassword)) {
+                UserDTO userDTO = userService.getByResetToken(token);
+                userDTO.setPassword(password);
+                userService.save(userDTO);
+                resetTokenService.deleteToken(token);
+
+            }
+            return "html_page";
         }
-
-        return "";
+        return "html_page_error";
     }
 }
